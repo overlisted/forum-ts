ed4826bdf1b3589402cdd3eed9be07757df40522
Ilya Maximov <mail@overlisted.net>
2019-08-02 00:39:20 +0300

    started creating login forms (WIP)

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,15 @@
-// @ts-ignore
-const express = require('express')
-const app = express();
+/*
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
 
-app.get('/', function(request: any, response: any) {
-  response.send('ура')
-});
-
-app.listen(4053);
+passport.use(new LocalStrategy(
+  function(username: any, password: any, done: any) {
+    User.findOne({ username: username }, function (err: any, user: any) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+)); */
