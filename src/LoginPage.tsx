cc043e2c50ec3dac7344fb389e686ef0ad80e798
True_han <matorassan@icloud.com>
2019-08-02 00:39:20 +0300

    started creating login forms (WIP)

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -3,9 +3,40 @@ import React from 'react';
 let LoginPage: React.FC = function () {
   return(
     <div className="content login">
-      <p>логин</p>
+      <LoginForm/>
     </div>
   )
 };
 
-export default LoginPage
+let RegisterForm: React.FC = function() {
+  return(
+      <form method="POST" action="http://localhost:4053/register">
+          <p>Имя пользователя</p>
+          <input name="username" type="email" autoComplete="on" required/>
+          <p>Адрес электронной почты</p>
+          <input name="email" type="email" autoComplete="on" required/>
+          <p>Пароль</p>
+          <input name="password" type="password" autoComplete="on" required/>
+          <p/>
+          <input type="submit" value="Войти"/>
+      </form>
+  )
+};
+
+let LoginForm: React.FC = function() {
+  return (
+    <form method="POST" action="http://localhost:4053/register">
+      <p>Адрес электронной почты</p>
+      <input name="email" type="email" autoComplete="on" required/>
+      <p>Пароль</p>
+      <input name="password" type="password" autoComplete="on" required/>
+      };
+
+      export default LoginPage
+      <p/>
+      <input type="submit" value="Войти"/>
+    </form>
+  );
+};
+
+export default LoginPage;
